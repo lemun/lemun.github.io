15c17ba8074d900e14755de3955af2c1deb67129
Shai Sinai <shai@tuta.com>
2025-09-10 12:58:32 +0300

    Updated resume

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lemun.github.io
+# Download PDF
+1. Open in browser
+3. Right click "resume PDF" > "Save link as..."
---
+++ script.js
@@ -0,0 +1,24 @@
+const url = new URL(window.location.href);
+if (url.searchParams.has("print")) {
+  const downloadLink = document.querySelector(".download-link");
+  downloadLink.style.display = "none";
+}
+
+function syncContactInfo(dataTestName) {
+  const headerElement = document.querySelector(
+    `[data-test-location="header"][data-test-name="${dataTestName}"]`
+  );
+  const sidebarElement = document.querySelector(
+    `[data-test-location="sidebar"][data-test-name="${dataTestName}"]`
+  );
+
+  if (headerElement && sidebarElement) {
+    headerElement.innerHTML = sidebarElement.innerHTML;
+  }
+}
+
+syncContactInfo("contact-location");
+syncContactInfo("contact-email");
+syncContactInfo("contact-github");
+syncContactInfo("contact-linkedin");
+syncContactInfo("contact-pdf");
